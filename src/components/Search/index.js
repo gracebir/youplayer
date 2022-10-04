082aa8f1a5ba87abc3f80aa9a81d2b05e3fbc279
gracebir <grace.devolop@gmail.com>
2022-10-04 16:59:12 +0200

    done navbar

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Search.css'
 
 function Search() {
+  const [search, setSearch] = useState("react")
+  const onSearch = (keyword) => {
+    console.log(keyword);
+  }
   return (
-    <div>
-      <h1>search</h1>
+    <div className='search__form'>
+      <input className='search__input dark__modify' placeholder='search' type="text" onChange={(e) => setSearch(e.target.value)} />
+      <button className='btn__search dark__modify' onClick={()=>onSearch(search)}>search</button>
     </div>
   )
 }
