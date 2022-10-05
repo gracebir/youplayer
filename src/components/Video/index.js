9489344b0ea23cd9ea11db189f8dd951dfd59044
gracebir <grace.devolop@gmail.com>
2022-10-05 10:06:58 +0200

    video desktop view

--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -1,9 +1,130 @@
 import React from 'react'
+import Videocard from '../VideoCard'
+import './Video.css';
 
 function Video() {
   return (
-    <div>
-      
+    <div className='video'>
+      <Videocard 
+      thumnail="https://i.ytimg.com/vi/mt81V-sS_So/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAE1rNWIZWXdB5p1plWtLeEq4o-fQ"
+      auther_img={"https://yt3.ggpht.com/ytc/AMLnZu-Bwb4KHlwhGRHSf0hreeqn0GfDOdfWgEgHH2qd=s68-c-k-c0x00ffffff-no-rj"}
+      title={"a REAL day in the life a 21 year old software engineer working from home"}
+      author="Michael Vandi"
+      />
+      <Videocard 
+      thumnail="https://i.ytimg.com/vi/mt81V-sS_So/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAE1rNWIZWXdB5p1plWtLeEq4o-fQ"
+      auther_img={"https://yt3.ggpht.com/ytc/AMLnZu-Bwb4KHlwhGRHSf0hreeqn0GfDOdfWgEgHH2qd=s68-c-k-c0x00ffffff-no-rj"}
+      title={"a REAL day in the life a 21 year old software engineer working from home"}
+      author="Michael Vandi"
+      />
+      <Videocard 
+      thumnail="https://i.ytimg.com/vi/mt81V-sS_So/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAE1rNWIZWXdB5p1plWtLeEq4o-fQ"
+      auther_img={"https://yt3.ggpht.com/ytc/AMLnZu-Bwb4KHlwhGRHSf0hreeqn0GfDOdfWgEgHH2qd=s68-c-k-c0x00ffffff-no-rj"}
+      title={"a REAL day in the life a 21 year old software engineer working from home"}
+      author="Michael Vandi"
+      />
+      <Videocard 
+      thumnail="https://i.ytimg.com/vi/mt81V-sS_So/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAE1rNWIZWXdB5p1plWtLeEq4o-fQ"
+      auther_img={"https://yt3.ggpht.com/ytc/AMLnZu-Bwb4KHlwhGRHSf0hreeqn0GfDOdfWgEgHH2qd=s68-c-k-c0x00ffffff-no-rj"}
+      title={"a REAL day in the life a 21 year old software engineer working from home"}
+      author="Michael Vandi"
+      />
+      <Videocard 
+      thumnail="https://i.ytimg.com/vi/mt81V-sS_So/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAE1rNWIZWXdB5p1plWtLeEq4o-fQ"
+      auther_img={"https://yt3.ggpht.com/ytc/AMLnZu-Bwb4KHlwhGRHSf0hreeqn0GfDOdfWgEgHH2qd=s68-c-k-c0x00ffffff-no-rj"}
+      title={"a REAL day in the life a 21 year old software engineer working from home"}
+      author="Michael Vandi"
+      />
+      <Videocard 
+      thumnail="https://i.ytimg.com/vi/mt81V-sS_So/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAE1rNWIZWXdB5p1plWtLeEq4o-fQ"
+      auther_img={"https://yt3.ggpht.com/ytc/AMLnZu-Bwb4KHlwhGRHSf0hreeqn0GfDOdfWgEgHH2qd=s68-c-k-c0x00ffffff-no-rj"}
+      title={"a REAL day in the life a 21 year old software engineer working from home"}
+      author="Michael Vandi"
+      />
+      <Videocard 
+      thumnail="https://i.ytimg.com/vi/mt81V-sS_So/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAE1rNWIZWXdB5p1plWtLeEq4o-fQ"
+      auther_img={"https://yt3.ggpht.com/ytc/AMLnZu-Bwb4KHlwhGRHSf0hreeqn0GfDOdfWgEgHH2qd=s68-c-k-c0x00ffffff-no-rj"}
+      title={"a REAL day in the life a 21 year old software engineer working from home"}
+      author="Michael Vandi"
+      />
+      <Videocard 
+      thumnail="https://i.ytimg.com/vi/mt81V-sS_So/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAE1rNWIZWXdB5p1plWtLeEq4o-fQ"
+      auther_img={"https://yt3.ggpht.com/ytc/AMLnZu-Bwb4KHlwhGRHSf0hreeqn0GfDOdfWgEgHH2qd=s68-c-k-c0x00ffffff-no-rj"}
+      title={"a REAL day in the life a 21 year old software engineer working from home"}
+      author="Michael Vandi"
+      />
+      <Videocard 
+      thumnail="https://i.ytimg.com/vi/mt81V-sS_So/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAE1rNWIZWXdB5p1plWtLeEq4o-fQ"
+      auther_img={"https://yt3.ggpht.com/ytc/AMLnZu-Bwb4KHlwhGRHSf0hreeqn0GfDOdfWgEgHH2qd=s68-c-k-c0x00ffffff-no-rj"}
+      title={"a REAL day in the life a 21 year old software engineer working from home"}
+      author="Michael Vandi"
+      />
+      <Videocard 
+      thumnail="https://i.ytimg.com/vi/mt81V-sS_So/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAE1rNWIZWXdB5p1plWtLeEq4o-fQ"
+      auther_img={"https://yt3.ggpht.com/ytc/AMLnZu-Bwb4KHlwhGRHSf0hreeqn0GfDOdfWgEgHH2qd=s68-c-k-c0x00ffffff-no-rj"}
+      title={"a REAL day in the life a 21 year old software engineer working from home"}
+      author="Michael Vandi"
+      />
+      <Videocard 
+      thumnail="https://i.ytimg.com/vi/mt81V-sS_So/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAE1rNWIZWXdB5p1plWtLeEq4o-fQ"
+      auther_img={"https://yt3.ggpht.com/ytc/AMLnZu-Bwb4KHlwhGRHSf0hreeqn0GfDOdfWgEgHH2qd=s68-c-k-c0x00ffffff-no-rj"}
+      title={"a REAL day in the life a 21 year old software engineer working from home"}
+      author="Michael Vandi"
+      />
+      <Videocard 
+      thumnail="https://i.ytimg.com/vi/mt81V-sS_So/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAE1rNWIZWXdB5p1plWtLeEq4o-fQ"
+      auther_img={"https://yt3.ggpht.com/ytc/AMLnZu-Bwb4KHlwhGRHSf0hreeqn0GfDOdfWgEgHH2qd=s68-c-k-c0x00ffffff-no-rj"}
+      title={"a REAL day in the life a 21 year old software engineer working from home"}
+      author="Michael Vandi"
+      />
+      <Videocard 
+      thumnail="https://i.ytimg.com/vi/mt81V-sS_So/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAE1rNWIZWXdB5p1plWtLeEq4o-fQ"
+      auther_img={"https://yt3.ggpht.com/ytc/AMLnZu-Bwb4KHlwhGRHSf0hreeqn0GfDOdfWgEgHH2qd=s68-c-k-c0x00ffffff-no-rj"}
+      title={"a REAL day in the life a 21 year old software engineer working from home"}
+      author="Michael Vandi"
+      />
+      <Videocard 
+      thumnail="https://i.ytimg.com/vi/mt81V-sS_So/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAE1rNWIZWXdB5p1plWtLeEq4o-fQ"
+      auther_img={"https://yt3.ggpht.com/ytc/AMLnZu-Bwb4KHlwhGRHSf0hreeqn0GfDOdfWgEgHH2qd=s68-c-k-c0x00ffffff-no-rj"}
+      title={"a REAL day in the life a 21 year old software engineer working from home"}
+      author="Michael Vandi"
+      />
+      <Videocard 
+      thumnail="https://i.ytimg.com/vi/mt81V-sS_So/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAE1rNWIZWXdB5p1plWtLeEq4o-fQ"
+      auther_img={"https://yt3.ggpht.com/ytc/AMLnZu-Bwb4KHlwhGRHSf0hreeqn0GfDOdfWgEgHH2qd=s68-c-k-c0x00ffffff-no-rj"}
+      title={"a REAL day in the life a 21 year old software engineer working from home"}
+      author="Michael Vandi"
+      />
+      <Videocard 
+      thumnail="https://i.ytimg.com/vi/mt81V-sS_So/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAE1rNWIZWXdB5p1plWtLeEq4o-fQ"
+      auther_img={"https://yt3.ggpht.com/ytc/AMLnZu-Bwb4KHlwhGRHSf0hreeqn0GfDOdfWgEgHH2qd=s68-c-k-c0x00ffffff-no-rj"}
+      title={"a REAL day in the life a 21 year old software engineer working from home"}
+      author="Michael Vandi"
+      />
+      <Videocard 
+      thumnail="https://i.ytimg.com/vi/mt81V-sS_So/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAE1rNWIZWXdB5p1plWtLeEq4o-fQ"
+      auther_img={"https://yt3.ggpht.com/ytc/AMLnZu-Bwb4KHlwhGRHSf0hreeqn0GfDOdfWgEgHH2qd=s68-c-k-c0x00ffffff-no-rj"}
+      title={"a REAL day in the life a 21 year old software engineer working from home"}
+      author="Michael Vandi"
+      />
+      <Videocard 
+      thumnail="https://i.ytimg.com/vi/mt81V-sS_So/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAE1rNWIZWXdB5p1plWtLeEq4o-fQ"
+      auther_img={"https://yt3.ggpht.com/ytc/AMLnZu-Bwb4KHlwhGRHSf0hreeqn0GfDOdfWgEgHH2qd=s68-c-k-c0x00ffffff-no-rj"}
+      title={"a REAL day in the life a 21 year old software engineer working from home"}
+      author="Michael Vandi"
+      />
+      <Videocard 
+      thumnail="https://i.ytimg.com/vi/mt81V-sS_So/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAE1rNWIZWXdB5p1plWtLeEq4o-fQ"
+      auther_img={"https://yt3.ggpht.com/ytc/AMLnZu-Bwb4KHlwhGRHSf0hreeqn0GfDOdfWgEgHH2qd=s68-c-k-c0x00ffffff-no-rj"}
+      title={"a REAL day in the life a 21 year old software engineer working from home"}
+      author="Michael Vandi"
+      />
+      <Videocard 
+      thumnail="https://i.ytimg.com/vi/mt81V-sS_So/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAE1rNWIZWXdB5p1plWtLeEq4o-fQ"
+      auther_img={"https://yt3.ggpht.com/ytc/AMLnZu-Bwb4KHlwhGRHSf0hreeqn0GfDOdfWgEgHH2qd=s68-c-k-c0x00ffffff-no-rj"}
+      title={"a REAL day in the life a 21 year old software engineer working from home"}
+      author="Michael Vandi"
+      />
     </div>
   )
 }
---
+++ src/components/VideoCard/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Videocard.css'
+
+function Videocard({
+    thumnail, title, author, auther_img
+}) {
+  return (
+    <div className='video__card'>
+      <div className="video__thumnail">
+        <img className='thumnail__img' src={thumnail} alt="thumnail" />
+      </div>
+      <div className='video__description'>
+        <img className='author__img' src={auther_img} alt="author" />
+        <div className='video__detail'>
+            <h5>{title}</h5>
+            <div>
+                <p className='author__name'>{author}</p>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Videocard
